2abf469d51bb204409bd14365ce08e8c32e6af2b
s-satsangi <s.satsangi@gmail.com>
2020-11-17 06:50:41 -0500

    there has to be a better way

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+//roommates.find( function(s) { return s === "winston" }
+function superbowlWin(team){
+  let win = team.find(function(game){return game.result === "W"});
+  return win ? win.year : win ;
+}
